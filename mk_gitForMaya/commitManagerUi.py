201994d85e6c423f669d278b68b1312ee8b355c0
kudiaborm <1Typewriter>
2014-11-17 00:56:27 -0500

    fixed gitForMaya missing from git

--- mk_gitForMaya/commitManagerUi.py
+++ mk_gitForMaya/commitManagerUi.py
@@ -5,22 +5,45 @@ import PyQt4.QtGui as QtGui
 
 import os
 
+
 def listdir_fullpath(d):
-	return [os.path.join(d, f) for f in os.listdir(d)]
+    return [os.path.join(d, f) for f in os.listdir(d)]
 
-class Window(QtGui.QMainWindow):
 
-	def __init__(self, parent = None):
-		super(Window, self).__init__(parent)
-		self.resize(800, 400)
-		self.move(300, 300)
-		self.setWindowTitle('Simple')
+class CommitManager(QtGui.QMainWindow):
+    def __init__(self, parent=None):
 
-		self.mainWidget = QtGui.QWidget(self)
-		self.mainWidget.setGeometry(0, 0, 800, 400)
-		self.mainLayout = QtGui.QVBoxLayout()
-		self.mainWidget.setLayout(self.mainLayout)
 
-		self.setStyleSheet("background: rgb(77, 80, 82)")
-	def createMenuBar(self):
-		pass
+        for qt in QtGui.QApplication([]).allWidgets():
+            if qt.__class__.__name__ == self.__class__.__name__:
+                qt.deleteLater()
+        super(Window, self).__init__(parent=parent)
+        self.resize(800, 400)
+        self.move(300, 300)
+        self.setWindowTitle('Simple')
+
+        self.mainWidget = QtGui.QWidget(self)
+        self.mainWidget.setGeometry(0, 0, 800, 400)
+        self.mainLayout = QtGui.QGridLayout()
+        self.mainWidget.setLayout(self.mainLayout)
+
+        self.setStyleSheet("background: rgb(77, 80, 82)")
+
+        self.commitNameField = QtGui.QLineEdit()
+        self.commitMessageField = QtGui.QLineEdit()
+
+        self.commitNameField.setPlaceholderText("Commit Name")
+        self.commitMessageField.setPlaceholderText("Commit Details")
+
+        self.commitButton = QtGui.QPushButton("Commit")
+        self.cancelButton = QtGui.QPushButton("Cancel")
+
+        self.layout().addWidget(self.commitNameField, 0, 0, 1, 2)
+        self.layout().addWidget(self.commitMessageField, 0, 1, 1, 2)
+        self.layout().addWidget(self.commitButton, 0, 2, 1, 1)
+        self.layout().addWidget(self.cancelButton, 0, 2, 1, 1)
+
+        self.show()
+
+    def createMenuBar(self):
+        pass
